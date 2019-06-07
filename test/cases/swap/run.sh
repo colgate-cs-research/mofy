7fd2df212e2bcffa794590dda2815d619a779d48
Xiaolin Sun <xsun@colgate.edu>
2019-06-07 14:04:15 -0400

    add test cases

---
+++ test/cases/swap/run.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+TEST_DIR="$(cd "$(dirname "$0")" && pwd)"
+source "$TEST_ROOT/common.sh"
+
+TMP_DIR="$(mktemp -d)"
+
+mofy_run -configs $TEST_DIR/configs -outputDir $TMP_DIR \
+    -Modification Swap -Percentage 100 -seed 1
+diff -r $TEST_DIR/expected $TMP_DIR
